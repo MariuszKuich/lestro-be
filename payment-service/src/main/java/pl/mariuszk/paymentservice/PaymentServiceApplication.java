77330651fd7e4ebdc6f35a25c13329c0ba58814a
MariuszKuich <mariuszkuich@outlook.com>
2022-11-25 20:43:08 +0100

    add missing @ConfigurationPropertiesScan annotation

--- payment-service/src/main/java/pl/mariuszk/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/pl/mariuszk/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package pl.mariuszk.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("pl.mariuszk.paymentservice.config")
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
