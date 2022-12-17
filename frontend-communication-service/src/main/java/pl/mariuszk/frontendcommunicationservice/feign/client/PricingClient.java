d7c102147834cf5229c764b56eb6b1b58199185e
MariuszKuich <mariuszkuich@outlook.com>
2022-12-17 10:59:23 +0100

    delete pricing microservice

--- frontend-communication-service/src/main/java/pl/mariuszk/frontendcommunicationservice/feign/client/PricingClient.java
+++
@@ -1,8 +0,0 @@
-package pl.mariuszk.frontendcommunicationservice.feign.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient("pricing-service")
-public interface PricingClient {
-
-}
